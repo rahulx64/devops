1d96adad1365b1ac1418a40056ad23f8c493068f
Rahul Ranjan <rahulxyad@gmail.com>
2025-11-28 10:55:03 +0530

    adding notes

--- script/first.sh
+++ script/first.sh
@@ -1,20 +1,8 @@
 #!/bin/bash
-echo "Hello, World!"
 
-
-# -bash: ./firstscript.sh
-# chmod +x myscript.sh
-
-# echo "This is my first script."
-# uptime
-# free -m
-# df -h
-
-echo "i am trying to learn scripting through practising but it seems it is impossible to  learn in two days despite i have good knowldege of linux commands"
-
-echo "THe upstime of system is "
-uptime
-echo "Memeory Utilization is as follows"
-free -m
-echo "Disk Utilization is as follows"
-df -h
+echo "CPU Temperature Monitoring:"
+if command -v sensors &> /dev/null; then
+     sensors | grep -i 'temp'  # Adjust based on your system's output
+else
+     echo "Please install 'lm-sensors' to monitor CPU temperature."
+fi
